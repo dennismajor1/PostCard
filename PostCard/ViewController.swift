183a0e7701bd7fe32187a4bbddc10b1c1fb4b68c
Dennis Major <dennismajor1@gmail.com>
2014-11-23 15:04:07 -0500

    Adds name label and associated swift code.

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +26,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
 
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+
     }
+
 }
 
